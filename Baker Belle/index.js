e8f5244a92006e31a40b19fb2348ce9b2c02d42d
Sola <hello@bysola.co>
2024-05-27 15:33:15 -0700

    Reset with basic tutorial from Discord

---
+++ Baker Belle/index.js
@@ -0,0 +1,25 @@
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits, SlashCommandBuilder } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once).
+// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
+// It makes some properties non-nullable.
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
+	async execute(interaction) {
+		await interaction.reply('Pong!');
+	},
+};
+
+// Log in to Discord with your client's token
+client.login(token);
